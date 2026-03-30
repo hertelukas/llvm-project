a9cbb0d01898130349bf7ddea19d6e63ee19ce86
Lukas <lukas.hertel@tum.de>
2026-03-30 17:09:06 +0200

    [FixIrreducible] delete all phi  occurences in outgoing blocks

--- llvm/lib/Transforms/Utils/ControlFlowUtils.cpp
+++ llvm/lib/Transforms/Utils/ControlFlowUtils.cpp
@@ -248,14 +248,17 @@ static void reconnectPhis(BasicBlock *Out, BasicBlock *GuardBlock,
     bool AllUndef = true;
     for (auto [BB, Succ0, Succ1] : Incoming) {
       Value *V = PoisonValue::get(Phi->getType());
-      if  (Phi->getBasicBlockIndex(BB) != -1) {
+      bool Found = false;
+      while (Phi->getBasicBlockIndex(BB) != -1) {
         V = Phi->removeIncomingValue(BB, false);
+        Found = true;
+      }
+      if (Found) {
         if (BB == Out) {
           V = NewPhi;
         }
         AllUndef &= isa<UndefValue>(V);
       }
-
       NewPhi->addIncoming(V, BB);
     }
     assert(NewPhi->getNumIncomingValues() == Incoming.size());
